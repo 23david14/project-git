5d3f211c48771145df5785fdc6bfcc94b8c1ef77
23david14 <david18martinez23@gmail.com>
2024-09-13 16:46:26 -0500

    Se agregó un archivo nuevo a la ramanueva

---
+++ file3.py
@@ -0,0 +1,4 @@
+from main import saludo, saludo_extra
+
+print(saludo_extra())
+print(saludo())
--- main.py
+++ main.py
@@ -3,5 +3,3 @@ from file2 import saludo_extra
 def saludo():
     return "Este archivo es el primero"
 
-print(saludo())
-print(saludo_extra())
